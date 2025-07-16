c784303bee404dd7d9a2cd3fc858215e95e5d4e8
Hiddle00 <59528807+Hiddle00@users.noreply.github.com>
2025-07-16 16:29:22 +0900

    010

---
+++ 0716/PrintTest.java
@@ -0,0 +1,16 @@
+public class PrintTest 
+{
+
+	public static void main(String[] args) 
+	{
+        boolean f;
+        int n = 1;
+        int x = 0;
+        int y = 0;
+
+        m = (++n + ++n) % 2;
+        f = 
+        System.out.println(m);
+
+    }
+}
